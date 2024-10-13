086faf549d1703b4e22e099a0a6854d8a8030075
kairith <159109160+kairith@users.noreply.github.com>
2024-10-13 15:27:22 +0700

    Update BONUS-1.dart

--- 2-START-CODE/BONUS-1.dart
+++ 2-START-CODE/BONUS-1.dart
@@ -11,8 +11,30 @@ void main() {
 
   // Process the instructions and get the final position and direction
   
+  for (var instructions in instructions.split("")) {
+      if(instructions == "R"){
+        direction = Direction.values[(direction.index + 1) % 4];
+      }else if(instructions == "L"){
+        direction = Direction.values[(direction.index - 1) % 4];
+      }else if(instructions == "A"){
+        switch(direction){
+          case Direction.north:
+          y++;
+          break;
+          case Direction.west:
+          y--;
+          break;
+          case Direction.south:
+          x--;
+          break;
+          case Direction.east:
+          x++;
+          break;
+        }
+      }
 
+      }
   // Print the final position and direction
-  print("Final position:  ");
-  print("Facing:  ");
+  print("Final position: x = $x y = $y ${direction.toString().split('.').last}");
+  
 }
