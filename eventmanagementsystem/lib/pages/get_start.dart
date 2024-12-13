b33c563464e491e2f52376beca366baef8d18306
channdararith <dararithzone@gmail.com>
2024-12-13 23:09:13 +0700

    firstcommit

--- eventmanagementsystem/lib/pages/get_start.dart
+++ eventmanagementsystem/lib/pages/get_start.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart'; // Import the flutter_svg package
 import 'package:eventmanagementsystem/pages/login_page.dart';
+
 class Getstart extends StatelessWidget {
   const Getstart({super.key});
 
@@ -12,18 +13,34 @@ class Getstart extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            SvgPicture.asset('assets/images/event-logo.svg'), // Updated line
+            SvgPicture.asset(
+              'assets/images/event-logo.svg',
+              width: 350,
+            ), // Updated line
             const SizedBox(height: 20),
-            const Text("Event Management System"),
+            const Text("Booking and Register Event...",
+                style: TextStyle(fontSize: 20, fontStyle: FontStyle.normal)),
             const SizedBox(height: 100),
-            ElevatedButton(onPressed: () {
+            ElevatedButton(
+              onPressed: () {
                 // Navigate to LoginPage
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => LoginPage()),
                 );
-              }, child: const Text("Get Started"),
-            ),
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue, // Background color
+                foregroundColor: Colors.white, // Text color
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 32.0, vertical: 16.0), // Padding
+                textStyle: const TextStyle(
+                  fontSize: 18, // Font size
+                  fontWeight: FontWeight.bold, // Font weight
+                ),
+              ),
+              child: const Text("Get Started"),
+            )
           ],
         ),
       ),
